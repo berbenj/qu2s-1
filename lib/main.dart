3a326f07f84fbd12f0e9e3e1d1a5e1b26a3b636b
7g94 <dev@qu2s.com>
2023-05-10 14:13:12 +0200

    login form

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:ntp/ntp.dart';
 import 'package:flutter/material.dart';
 import 'package:dart_date/dart_date.dart';
@@ -10,17 +9,32 @@ import 'package:cloud_firestore/cloud_firestore.dart' as fs;
 import 'firebase_options.dart';
 
 import 'q2_date.dart';
+import 'q2_platform.dart';
+
+Q2Platform q2Platform = Q2Platform();
 
 void main() {
-  if (kIsWeb) {
-    Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  } else if (defaultTargetPlatform == TargetPlatform.windows) {
-    fd.Firestore.initialize('qu2s-e9232');
-  } else {
-    return;
-    // todo...
-  }
+  connectToDatabase();
   runApp(const Qu2sApp());
+}
+
+// fixme: extract database reletad things to a seperate libriray
+// firestore
+// - can crud all collection, all doc,
+// - can stream any collection, doc
+// - can use a fake db for testing
+// auth
+// - can do everything
+// storage
+// - can crud anything
+void connectToDatabase() async {
+  if (q2Platform.isWeb) {
+    Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  } else if (q2Platform.isWindows) {
+    var app = fd.Firestore.initialize('qu2s-e9232');
+  } else {
+    // todo: implement for android (linux, macOS, iOS)
+  }
 }
 
 class Qu2sApp extends StatelessWidget {
@@ -36,18 +50,22 @@ class Qu2sApp extends StatelessWidget {
           scaffoldBackgroundColor: const Color(0xFF0a0a0a),
           primaryColor: Colors.white,
           fontFamily: "Roboto",
+          textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.white)),
+          inputDecorationTheme: const InputDecorationTheme(
+            border: UnderlineInputBorder(),
+            enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+          ),
           scrollbarTheme: const ScrollbarThemeData(
             thumbColor: MaterialStatePropertyAll(Color(0xff444444)),
             minThumbLength: 30,
             trackVisibility: MaterialStatePropertyAll(false),
           )),
       home: DefaultTabController(
-          length: 2,
+          length: 3,
           child: Scaffold(
             appBar: q2AppBar(),
             body: q2AppBody(),
           )),
-      //const HomePage(title: 'Home Page'),
     );
   }
 
@@ -55,8 +73,9 @@ class Qu2sApp extends StatelessWidget {
     return const TabBarView(
       physics: NeverScrollableScrollPhysics(),
       children: [
-        HomePage(title: 'Home Page'),
+        HomePage(),
         CalendarPage(),
+        LoginPage(),
       ],
     );
   }
@@ -64,24 +83,12 @@ class Qu2sApp extends StatelessWidget {
   AppBar q2AppBar() {
     return AppBar(
       elevation: 5,
-      title: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: const [
-            Icon(Icons.signpost),
-            Text(
-              " qu2s",
-              style: TextStyle(color: Colors.white, fontSize: 18, fontFamily: "Fira code", fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-      ),
       toolbarHeight: 0,
       backgroundColor: const Color(0xFF0a0a0a),
       bottom: const PreferredSize(
         preferredSize: Size.fromHeight(kBottomNavigationBarHeight),
         child: SizedBox(
-          width: 200,
+          width: 300,
           child: TabBar(
               indicatorSize: TabBarIndicatorSize.label,
               labelColor: Color(0xFFaaaaaa),
@@ -90,7 +97,55 @@ class Qu2sApp extends StatelessWidget {
               tabs: [
                 Tab(text: "Home"),
                 Tab(text: "Calendar"),
+                Tab(text: 'Login'),
               ]),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints(maxWidth: 200),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TextField(
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.zero,
+                labelStyle: TextStyle(color: Colors.white38),
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 20),
+            const TextField(
+              style: TextStyle(color: Colors.white),
+              obscureText: true,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.zero,
+                labelStyle: TextStyle(color: Colors.white38),
+                labelText: 'Password',
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  // todo: login
+                },
+                child: const Text('Login'))
+          ],
         ),
       ),
     );
@@ -111,9 +166,9 @@ class _CalendarPageState extends State<CalendarPage> {
   Widget build(BuildContext context) {
     late CollectionReference eventCollectionWeb;
     late fd.CollectionReference eventCollection;
-    if (kIsWeb) {
+    if (q2Platform.isWeb) {
       eventCollectionWeb = fs.FirebaseFirestore.instance.collection("event_test");
-    } else if (defaultTargetPlatform == TargetPlatform.windows) {
+    } else if (q2Platform.isWindows) {
       eventCollection = fd.Firestore.instance.collection("event_test");
     }
 
@@ -128,9 +183,9 @@ class _CalendarPageState extends State<CalendarPage> {
                 backgroundColor: MaterialStateProperty.all<Color>(Colors.blueGrey),
               ),
               onPressed: () async {
-                if (kIsWeb || defaultTargetPlatform == TargetPlatform.android) {
+                if (q2Platform.isWeb) {
                   await eventCollectionWeb.doc("event").update({"is_event": true});
-                } else if (defaultTargetPlatform == TargetPlatform.windows) {
+                } else if (q2Platform.isWindows) {
                   await eventCollection.document("event").update({"is_event": true});
                 }
               },
@@ -142,9 +197,9 @@ class _CalendarPageState extends State<CalendarPage> {
                 backgroundColor: MaterialStateProperty.all<Color>(Colors.blueGrey),
               ),
               onPressed: () async {
-                if (kIsWeb || defaultTargetPlatform == TargetPlatform.android) {
+                if (q2Platform.isWeb) {
                   await eventCollectionWeb.doc("event").update({"is_event": false});
-                } else if (defaultTargetPlatform == TargetPlatform.windows) {
+                } else if (q2Platform.isWindows) {
                   await eventCollection.document("event").update({"is_event": false});
                 }
               },
@@ -164,7 +219,7 @@ class _CalendarPageState extends State<CalendarPage> {
           height: 200,
           width: 200,
           child: Center(
-              child: (kIsWeb)
+              child: (q2Platform.isWeb)
                   ? StreamBuilder(
                       stream: eventCollectionWeb.doc("event").snapshots(),
                       builder: (context, snapshot) {
@@ -178,19 +233,24 @@ class _CalendarPageState extends State<CalendarPage> {
                         return Container();
                       },
                     )
-                  : StreamBuilder(
-                      stream: eventCollection.document("event").stream,
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData && snapshot.data!["is_event"]) {
-                          return Container(
-                            width: 180,
-                            height: 180,
-                            decoration: const BoxDecoration(color: Colors.orange),
-                          );
-                        }
-                        return Container();
-                      },
-                    )),
+                  : (q2Platform.isWindows)
+                      ? StreamBuilder(
+                          stream: eventCollection.document("event").stream,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData && snapshot.data!["is_event"]) {
+                              return Container(
+                                width: 180,
+                                height: 180,
+                                decoration: const BoxDecoration(color: Colors.orange),
+                              );
+                            }
+                            return Container();
+                          },
+                        )
+                      // if it is not a supported platform
+                      : const Center(
+                          child: Text("This platform is not supported!"),
+                        )),
         ),
       ],
     );
@@ -198,9 +258,7 @@ class _CalendarPageState extends State<CalendarPage> {
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-
-  final String title;
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -214,8 +272,8 @@ class _HomePageState extends State<HomePage> {
   String _gregorianHour = "";
   String _gregorianMinute = "";
   String _gregorianSecond = "";
-  final Color _textColor = const Color.fromARGB(255, 228, 228, 228);
-  Timer? _timer;
+  Timer? _clockTimer;
+  Timer? _aNTPTimer;
   int offset = 0;
   final ScrollController _scrollController = ScrollController();
 
@@ -228,7 +286,7 @@ class _HomePageState extends State<HomePage> {
 
       Q2Date qdt = Q2Date(date);
 
-      // TODO: make seperation characters costumizable
+      // todo: make seperation characters costumizable
       _b36Date = "${qdt.b36Year}-${qdt.b36Pernor}${qdt.b36Scrop}-${qdt.b36Day}";
       _mtArton = "${qdt.mtArton}'";
       _mtJitt = " ${qdt.mtJitt}\"";
@@ -244,13 +302,14 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     _getTime(DateTime.now());
-    _timer = Timer.periodic(const Duration(milliseconds: 10), (Timer t) => _setCurrentDate());
-    _timer = Timer.periodic(const Duration(seconds: 10), (Timer t) => _getTime(DateTime.now()));
+    _clockTimer = Timer.periodic(const Duration(milliseconds: 10), (Timer t) => _setCurrentDate());
+    _aNTPTimer = Timer.periodic(const Duration(seconds: 10), (Timer t) => _getTime(DateTime.now()));
   }
 
   @override
   void dispose() {
-    _timer?.cancel();
+    _clockTimer!.cancel();
+    _aNTPTimer!.cancel();
     super.dispose();
   }
 
@@ -286,23 +345,23 @@ class _HomePageState extends State<HomePage> {
                         b36Clock(),
                       ]),
                 ),
-                post("1k6/33-c", [
+                post("1k6-33-c", [
                   "First of all, unfortuniatly I didn't manage to progress much these times. Partly because of work, partly because of my own lazyness getting the best of me. But still I will write down the little progress I made these 2 scrop.",
                   "I decided to post a scroply update, in a devblog style. I will do this as part of me ending the sprint by presenting the developments of that sprint to the stakeholders. It will mean a consistent update for anyone who is following the project, and a good motivation to not drift off and have something that I can actually present at the end of each sprint. (also, I'm aligning the sprints with scrops)",
                   "I also decided to not continue working on the scheduling allgorithm, as refining it is not critical now. Rather focus on the things that are most critical and give the most value. And as of right now the app is not connecting to a database. After looking around I wanted to connect it to Firebase, but there is a problem. I want this app to run on windows, but Firebase's Flutter package by default doesn't connect to windows. I was looking around and found possible solutions, but I didn't manage to make it work so far.",
                   "Anyways, connecting to a database will be the next step, after that I want to refactor my code a bit, and also start to write testing for my code, as you can never start that too early.",
                 ]),
-                post("1k6/31-b", [
+                post("1k6-31-b", [
                   "The more I'm thinking about how to solve this shuffeling problem, the more complicated it gets. Though it probably doesn't help, that I also am coming up with more and more ideas for costumization, and also features that I want my algorithm to be capable of doing.",
                   "But I'm sure it will work out in the end. And after I figure out how I really want it to work I will try to explain it, cos right now you only know that it tries to ararnges events in a calendar \"optimally\". Untill then, look forward to it, beacause I have never seen a system similar to this one, and I plan to release it for free Though right now ther is not even a platform for manual event placing/watching, so I have to do that as well."
                 ]),
-                post("1k6/31-a", [
+                post("1k6-31-a", [
                   "A few days back I started working on an algorithm that can arrange events in a calendar automatically the best way possible. Ultimatelly I want to have all kinds of features like: multi level availability, priority, breakable tasks, balancing, repeating, and others; but now I just want to make the most simple one as reliable as possible.",
                   "For this I came up with four methods that can help placing these events, these are: order them for least flexibility, pushing events up and down to make space, swap events, and grouping free spaces while planning, and spreading them after it's done. Reading back to what I just wrote I don't think any of them completely understandable, but whatever, I know what I want to do. I completed the ordering, and finished the first case for the second method, and I think this will work out.",
                   "Actually my main concern is that my ideas of trying to place and move around these events may be flawed. I tried to look for similar problems but I couldn't find any. I hope that I will not miss anything while implementing these methods, and that my methods will give a pretty good result.",
                   "I also improved on the time accuricy with the help of NTP (Network Time Protocol).",
                 ]),
-                post("1k6/25-7", [
+                post("1k6-25-7", [
                   "A start ... yet again.",
                   "I truely hope I will not loose this start. I have started this project so many times, but I have always got lost in it, got off track or moved to a different platform to make this a reality. Not long before this I started to look for something that could support my plan of releaseing this to the web, on mibile and on desktop as well, and I decided on flutter. I really hope I will not regret this decision too soon.",
                   "I have a lot of plans on what and how I want to achive with qu2s (maybe even too much), but I don't like to talk about plans, because they can change so much in development. Though maybe it would be good to document them in a way so that I can see how things changed.",
@@ -321,18 +380,18 @@ class _HomePageState extends State<HomePage> {
       children: [
         Text(
           _b36Date,
-          style: TextStyle(color: _textColor, fontSize: 20, fontWeight: FontWeight.w300),
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
         ),
         Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               _mtArton,
-              style: TextStyle(color: _textColor, fontSize: 34, fontWeight: FontWeight.w900),
+              style: const TextStyle(fontSize: 34, fontWeight: FontWeight.w900),
             ),
             Text(
               _mtJitt,
-              style: TextStyle(color: _textColor, fontSize: 20, fontWeight: FontWeight.w300, height: 1.4),
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w300, height: 1.4),
             ),
           ],
         ),
@@ -345,22 +404,22 @@ class _HomePageState extends State<HomePage> {
       children: [
         Text(
           _gregorianDate,
-          style: TextStyle(color: _textColor, fontSize: 20, fontWeight: FontWeight.w300),
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
         ),
         Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               _gregorianHour,
-              style: TextStyle(color: _textColor, fontSize: 34, fontWeight: FontWeight.w900),
+              style: const TextStyle(fontSize: 34, fontWeight: FontWeight.w900),
             ),
             Text(
               _gregorianMinute,
-              style: TextStyle(color: _textColor, fontSize: 34, fontWeight: FontWeight.w300),
+              style: const TextStyle(fontSize: 34, fontWeight: FontWeight.w300),
             ),
             Text(
               _gregorianSecond,
-              style: TextStyle(color: _textColor, fontSize: 20, fontWeight: FontWeight.w300, height: 1.4),
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w300, height: 1.4),
             ),
           ],
         ),
@@ -389,15 +448,14 @@ class _HomePageState extends State<HomePage> {
   Text postParagraph(String par) {
     return Text(
       "\n$par",
-      style:
-          TextStyle(color: _textColor, fontSize: 14, fontFamily: "Fira code", fontWeight: FontWeight.w300, height: 1.5),
+      style: const TextStyle(fontSize: 14, fontFamily: "Fira code", fontWeight: FontWeight.w300, height: 1.5),
     );
   }
 
   Text postTitle(String title) {
     return Text(
       " $title \n${"-" * (title.length + 2)}",
-      style: TextStyle(color: _textColor, fontSize: 18, fontFamily: "Fira code", fontWeight: FontWeight.bold),
+      style: const TextStyle(fontSize: 18, fontFamily: "Fira code", fontWeight: FontWeight.bold),
     );
   }
 }
